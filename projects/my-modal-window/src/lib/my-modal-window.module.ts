64078d06d1d5c0641899428341e04bbf83a424a3
Yonatan <kra.yonatan@gmail.com>
2018-10-07 15:06:27 +0300

    Define the angular element

--- projects/my-modal-window/src/lib/my-modal-window.module.ts
+++ projects/my-modal-window/src/lib/my-modal-window.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import { MyModalWindowComponent } from './my-modal-window.component';
+import {createCustomElement} from "@angular/elements";
 
 @NgModule({
   imports: [
   ],
   declarations: [MyModalWindowComponent],
-  exports: [MyModalWindowComponent]
+  exports: [MyModalWindowComponent],
+  entryComponents: [MyModalWindowComponent]
 })
-export class MyModalWindowModule { }
+export class MyModalWindowModule {
+  constructor(injector: Injector) {
+    const ModalWindowHTMLElement = createCustomElement(MyModalWindowComponent, {injector: injector});
+    window.customElements.define('my-modal-window', ModalWindowHTMLElement);
+    console.log('Element has been registered');
+  }
+}
