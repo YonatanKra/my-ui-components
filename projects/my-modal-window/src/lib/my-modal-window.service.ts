a01784a4cec072b7872f2de2bcd682b0f6ef3226
Yonatan <kra.yonatan@gmail.com>
2018-10-07 12:38:07 +0300

    Appends my-modal-window only once to the DOM

--- projects/my-modal-window/src/lib/my-modal-window.service.ts
+++ projects/my-modal-window/src/lib/my-modal-window.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class MyModalWindowService {
 
   constructor() { }
+
+  open(config?: MyModalWindowConfig) {
+    let element = document.body.querySelector('my-modal-window');
+    if (!element) {
+      element = document.createElement('my-modal-window');
+    }
+    document.body.appendChild(element);
+  }
 }
