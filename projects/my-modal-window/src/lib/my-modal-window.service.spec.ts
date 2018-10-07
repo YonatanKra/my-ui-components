2cea96260f8b44f7a42feb816c1feddcff6019c5
Yonatan <kra.yonatan@gmail.com>
2018-10-07 12:36:12 +0300

    Test: Should add my-modal-window to the DOM only once

--- projects/my-modal-window/src/lib/my-modal-window.service.spec.ts
+++ projects/my-modal-window/src/lib/my-modal-window.service.spec.ts
@@ -3,10 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { MyModalWindowService } from './my-modal-window.service';
 
 describe('MyModalWindowService', () => {
+  let service: MyModalWindowService;
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: MyModalWindowService = TestBed.get(MyModalWindowService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    service = TestBed.get(MyModalWindowService);
+  });
+
+  describe('open', () => {
+    it('should append my-modal-window component to body if not initialized already', () => {
+      const nElementsBefore = document.querySelectorAll('my-modal-window').length;
+      service.open();
+      service.open();
+      expect(nElementsBefore).toEqual(0);
+      expect(document.querySelectorAll('my-modal-window').length).toEqual(1);
+    });
+
+  afterEach(() => {
+    document.body.removeChild(document.body.querySelector('my-modal-window'));
   });
 });
