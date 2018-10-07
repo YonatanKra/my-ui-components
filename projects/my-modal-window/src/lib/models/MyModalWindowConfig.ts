1e2598af9b2dc0e8964879d906f48a989194d5f9
Yonatan <kra.yonatan@gmail.com>
2018-10-07 12:43:24 +0300

    Define the MyModalWindowConfig model
    Cast the element according to our API
    Set the config property on the element

---
+++ projects/my-modal-window/src/lib/models/MyModalWindowConfig.ts
@@ -0,0 +1,6 @@
+export class MyModalWindowConfig {
+  content: string;
+  backgroundColor: string;
+  width: number;
+  height: number;
+}
--- projects/my-modal-window/src/lib/my-modal-window.service.ts
+++ projects/my-modal-window/src/lib/my-modal-window.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import {MyModalWindowConfig} from './models/MyModalWindowConfig';
+import {NgElement, WithProperties} from "@angular/elements";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyModalWindowService {
-
   constructor() { }
 
   open(config?: MyModalWindowConfig) {
-    let element = document.body.querySelector('my-modal-window');
+    let element = document.body.querySelector('my-modal-window') as NgElement & WithProperties<{config: MyModalWindowConfig}>;;
     if (!element) {
-      element = document.createElement('my-modal-window');
+      element = document.createElement('my-modal-window') as NgElement & WithProperties<{config: MyModalWindowConfig}>;;
     }
+    element.config = config;
     document.body.appendChild(element);
     return element;
   }
