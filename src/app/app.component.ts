3d42243d46dec080ca0046f782e26bba7a55a876
Yonatan <kra.yonatan@gmail.com>
2018-10-07 15:22:45 +0300

    Use in the test app: Bug in production?!?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
+import {MyModalWindowConfig} from "../../projects/my-modal-window/src/lib/models/MyModalWindowConfig";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-ui-components';
+
+  constructor(private element: ElementRef) {
+
+  }
+
+  openModal() {
+    const modal = document.createElement('my-modal-window');
+    const config = new MyModalWindowConfig();
+    config.content = '<h1>Hello Angular-IL</h1>';
+
+    modal.config = config;
+    this.element.nativeElement.appendChild(modal);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MyModalWindowModule} from 'my-modal-window';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyModalWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
